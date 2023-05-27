126b1df6fb8974056fc98b33450bcd26146c78f1
kirub-getch <chaoskirub@gmail.com>
2023-05-27 19:46:59 +0300

    commit all

--- passanger_registry.cpp
+++ passanger_registry.cpp
@@ -1,48 +1,31 @@
 #include<iostream>
-#include<cstring>
+#include<string>
+#include "ticket.h"
+#include <cstring>
 
 using namespace std ;
 
-struct ticketService {
-    char firstName[25];
-    char lastName[25];
-    char startingPlace[25];
-    char destinationPlace[25];
-    char IdNumber[20];
-    char signature[10];
-};
+
 
 
 
 int main() {
   
-    ticketService persons[10];//structure variable declaration
+cout<<"\tWelcome To Our Ticket Service\n";
+  passanger_detail passanger_1;
+    
+    passanger_1.firstName = "kirub";
+    passanger_1.lastName = "getch";
+    passanger_1.phoneNumber = "0930630075";
+    passanger_1.emailAdress ="kirub@gmail.com";
 
-    cout<<"\tWelcome To Our Ticket Service\n";
+  
+    cout << "\tDear Customer Here is Your Ticket\n";
+    cout << "Your Full Name......\t\t" << passanger_1.firstName + passanger_1.lastName);
+    cout << "\nYour phone number...........\t" <<passanger_1.phoneNumber;
+    cout << "\nYour email adress.......\t" <<passanger_1.emailAdress;
+    
+    
 
-    for(int i=0; i<1; i++) {
-        cout << "passanger detail of passanger:"<< i + 1<<"\n";
-        cout<<"Enter Your First Name\n";
-        cin>>persons[i].firstName;
-        cout<<"Enter Your Last Name\n";
-        cin>>persons[i].lastName;
-        cout<<"Enter The Starting Place\n";
-        cin>>persons[i].startingPlace;
-        cout<<"Enter The Destination Place\n";
-        cin>>persons[i].destinationPlace;
-        cout<<"Enter Your Id number\n";
-        cin>>persons[i].IdNumber;
-        cout<<"Enter Owner Signature\n";
-        cin>>persons[i].signature;
-    }
-
-    for(int i=0; i <1; i++) {
-        cout<<"\tDear Customer Here is Your Ticket\n";
-        cout<<"Your Full Name......\t\t"<<strcat(persons[i].firstName , persons[i].lastName);
-        cout<<"\nYour Starting Place...........\t"<<persons[i].startingPlace;
-        cout<<"\nYour Destination Place.......\t"<<persons[i].destinationPlace;
-        cout<<"\nYour Id Number....\t"<<persons[i].IdNumber;
-        cout<<"\nOur Signature.......\t\t"<<persons[i].signature;
-    }
     return 0;
 }
