e5054de14a94359f72ea464762870d677f322314
kirub-getch <chaoskirub@gmail.com>
2023-05-27 21:42:43 +0300

    save all changes

--- passanger_registry.cpp
+++ passanger_registry.cpp
@@ -5,25 +5,6 @@
 
 using namespace std ;
 
-void travel_destinations(destinations *passanger)
-{
-  string arrival;
-
-  cout << "please enter your arival city :";
-  getline(cin, arrival);
-
-  for (int i = 0; i < 10; i++)
-  {
-    if (destinationRecord[i].city == arrival)
-    {
-      passanger->city = destinationRecord[i].city;
-      passanger->distance = destinationRecord[i].distance;
-      passanger->direction = destinationRecord[i].direction;
-    }
-    else 
-    cout<<"please enter the city's name correctly , dont forget to start with a capital letter"
-  }
-}
 
 int main()
  {
--- travel_destination.cpp
+++ travel_destination.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
 #include <string>
-//#include "ticket.h"
+#include "ticket.h"
 
 using namespace std;
 
-destinations travel_destinations(destinations *passanger)
+void travel_destinations(destinations *passanger)
 {
     string arrival;
 
-    cout<<"please enter your arival city :";
-    getline( cin , arrival);
+    cout << "please enter your arival city :";
+    getline(cin, arrival);
 
-    for(int i = 0; i < 10; i++)
-     {
-         if (destinationRecord[i].city == arrival)
-         {
+    for (int i = 0; i < 10; i++)
+    {
+        if (destinationRecord[i].city == arrival)
+        {
             passanger->city = destinationRecord[i].city;
             passanger->distance = destinationRecord[i].distance;
             passanger->direction = destinationRecord[i].direction;
-         }
-     }
-
+        }
+        else if (i == 9 && destinationRecord[i].city != arrival)
+        {
+            cout << "please enter the city's name correctly , dont forget to start with a capital letter\n";
+            break;
+        }
+    }
 }
